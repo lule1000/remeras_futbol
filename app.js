dcbac5726cf57b3fe81a6a89bd7e8dfd2b7005f2
Ulises Catarinolo <ulisescatarinolo@outlook.com>
2022-05-22 19:34:58 -0300

    subida del index

---
+++ app.js
@@ -0,0 +1,9 @@
+/*ingrese sunombre*/
+let nombreUsuario= prompt ('ingrese su nombre')
+/*ingrese un numero*/
+let numero1= Number(prompt('ingrese un numero'))
+/*ingrse otro numero*/
+let numero2= Number(prompt('ingrese otro numero'))
+alert(numero1+numero2)
+alert('HOLA'+""+nombreUsuario);
+
