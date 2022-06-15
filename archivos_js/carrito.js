156ecaf0e28cd5f2e661d38f9efa4045a0b333f2
Ulises Catarinolo <ulisescatarinolo@outlook.com>
2022-06-15 16:52:37 -0300

    actualizacion 15/06/22

--- archivos_js/carrito.js
+++ archivos_js/carrito.js
@@ -1,33 +1,43 @@
-const carrito = JSON.parse (localStorage.getItem ('carrito'));
+//TABLA CON DETALLES DE LA COMPRA
+const carrito = JSON.parse(localStorage.getItem('carrito'));
 
-let tbody = document.querySelector ('#tbody');
+let tbody = document.querySelector('#tbody');
 
-function rellenarCarrito (arrayCarrito){
+function rellenarCarrito(arrayCarrito) {
     for (let producto of arrayCarrito) {
-        let tr = document.createElement ('tr');
+        let tr = document.createElement('tr');
         tr.innerHTML = `<td>${producto.nombre}</td>
                          <td>$${producto.precio}</td>
                          <td>${producto.cantidad}</td>
                          <td>$${producto.subtotal}</td>
                          <td><button id='${producto.id}' class='btn btn-danger eliminarProducto'>Delete</button></td>`;
-    tbody.appendChild (tr);
+        tbody.appendChild(tr);
     }
 }
 
-rellenarCarrito (carrito);
+rellenarCarrito(carrito);
 
-let botonesEliminar = document.querySelectorAll ('.eliminarProducto');
+//TOTAL DE LA COMPRA
+let calcularTotal = carrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0);
 
-botonesEliminar.forEach (elemento => {
-    elemento.addEventListener ('click', eliminarProducto);
+let btnComprar = document.createElement('p');
+btnComprar.innerHTML = `<button class="bgVerde text-white ms-1 p-1 rounded-2">TOTAL $${calcularTotal}</button>`;
+document.body.appendChild(btnComprar);
+
+//ELIMINAR PRODUCTO
+
+let botonesEliminar = document.querySelectorAll('.eliminarProducto');
+
+botonesEliminar.forEach(elemento => {
+    elemento.addEventListener('click', eliminarProducto);
 })
 
-function eliminarProducto (e) {
-    let index = carrito.findIndex (producto => producto.id == e.target.id);
-    
-    carrito.splice (index, 1);
+function eliminarProducto(e) {
+    let index = carrito.findIndex(producto => producto.id == e.target.id);
 
-    e.target.parentNode.parentNode.remove ();
+    carrito.splice(index, 1);
 
-    localStorage.setItem ('carrito', JSON.stringify (carrito))
+    e.target.parentNode.parentNode.remove();
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
